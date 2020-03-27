5efb529754c0a45eb2b679a3a4952fe5ee1b2ef2
Elizabeth Tackett <emctackett@gmail.com>
2020-03-27 08:09:50 -0500

    initialize server.js with basic port listen and home path

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  console.log('home');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
