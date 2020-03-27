689e70f8fbc0b40003e7c521295d94d6d24ce2aa
Elizabeth Tackett <emctackett@gmail.com>
2020-03-27 13:24:45 -0500

    add script for room & init streaming

---
+++ public/js/room.js
@@ -0,0 +1,21 @@
+$(function() {
+  function streamVideo() {
+    navigator.getUserMedia(
+      { video: true, audio: true },
+      stream => {
+        const localVideo = $('#local-video')[0];
+        console.log(localVideo);
+        if (localVideo) {
+          localVideo.srcObject = stream;
+        }
+      },
+      error => {
+        console.warn(error.message);
+      }
+    );
+  }
+
+  if ($('.video-container')) {
+    streamVideo();
+  }
+});
