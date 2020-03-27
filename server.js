3199ac2309d9866798c5a3f45d30fd4fc2e6e633
Elizabeth Tackett <emctackett@gmail.com>
2020-03-27 12:37:02 -0500

    error handle routes 404 500

--- server.js
+++ server.js
@@ -21,6 +21,18 @@ app.get('/:room_id', (req, res) => {
   res.render('room');
 });
 
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
