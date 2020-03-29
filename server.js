eb935cab7bda91e5007c705ac51ae57cc32f40d2
Mae <laprestm@oregonstate.edu>
2020-03-29 11:05:48 -0500

    split getroom into getroom and getroomdetails (where no pull for text occurs) and now does not send verification email w/ empty email string

--- server.js
+++ server.js
@@ -28,6 +28,21 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('mysql', mysql);
 
+function getRoomDetails(res, mysql, context, id, complete){
+  var sql = "SELECT title, reader, listener, UIUD, email, start_time FROM sessions WHERE UIUD = ?";
+  var inserts = [id];
+  console.log(inserts)
+  mysql.pool.query(sql, inserts, function(error,results,fields){
+    if(error){
+      res.write(JSON.stringify(error));
+      res.end();
+    }
+    context.room = results[0];
+    console.log(context);
+    complete();
+  });
+}
+
 function getRoom(res, mysql, context, id, complete){
   var sql = "SELECT title, reader, listener, UIUD, email, start_time FROM sessions WHERE UIUD = ?";
   var inserts = [id];
@@ -45,7 +60,7 @@ function getRoom(res, mysql, context, id, complete){
 
 function getText(title, context, complete) {
   var storyFile = "public/txt/" + title.toUpperCase().replace(/ /g,'_') + ".txt"
-  console.log(storyFile);
+//  console.log(storyFile);
   fs.readFile(storyFile, 'utf8', function(err, data) {
     var storyText = [];
     var storyTitle = "";
@@ -97,7 +112,7 @@ function getText(title, context, complete) {
 
     context.title = storyTitle;
     context.text = storyText;
-    console.log(context);
+//    console.log(context);
     complete()
   });
 }
@@ -118,12 +133,9 @@ app.get('/schedule', (req, res) => {
 app.post('/schedule', function(req, res){
   var callbackCount = 0;
   var context = {};
-  console.log("post request received");
-  console.log(req.body)
   var mysql = req.app.get('mysql');
   var sql = "INSERT INTO sessions (UIUD, title, email, start_time, reader, listener) VALUES (?,?,?,?,?,?)";
   var inserts = [req.body.UIUDid, req.body.storyTitle, req.body.emailAddr, req.body.meetingTime, req.body.readId, req.body.listenId];
-  console.log(inserts);
   var message = {
     from: "reading2020room@gmail.com",
     to: req.body.emailAddr,
@@ -131,7 +143,6 @@ app.post('/schedule', function(req, res){
     text: 'Hello, You have signed up for a reading session at '+req.body.meetingTime+'. If you are the listener, you may reach your room at: https://reading-room.herokuapp.com/listenerRoom/'+req.body.UIUDid+'  If you are the reader, you may reach your room at:  https://reading-room.herokuapp.com/readerRoom/'+req.body.UIUDid+'  Have a wonderful day! Best, The Reading Room Team',
     html: '<p>Hello,</p><br><p>You have signed up for a reading session at '+req.body.meetingTime+'.</p><p>If you are the listener, you may reach your room at:<br><br><a href="https://reading-room.herokuapp.com/listenerRoom/'+req.body.UIUDid+'">https://reading-room.herokuapp.com/listenerRoom/'+req.body.UIUDid+'</a><br><br>If you are the reader, you may reach your room at:<br><a href="https://reading-room.herokuapp.com/readerRoom/'+req.body.UIUDid+'">https://reading-room.herokuapp.com/readerRoom/'+req.body.UIUDid+'</a></p><br>Have a wonderful day!<br><br>Best,<br>The Reading Room Team'
   };
-  console.log(message)
   sql = mysql.pool.query(sql,inserts,function(error, results, fields){
       if(error){
           console.log(JSON.stringify(error))
@@ -139,15 +150,16 @@ app.post('/schedule', function(req, res){
           res.end();
       }else{
         console.log(context)
-        getRoom(res, mysql, context, req.body.UIUDid, complete);
+        getRoomDetails(res, mysql, context, req.body.UIUDid, complete);
         function complete(){
           callbackCount++;
           if(callbackCount >= 1){
-            transporter.sendMail(message, function (err, info) {
-             if(err){console.log(err)}
-             else{console.log(info);}
-            });
-            console.log(context);
+            if(req.body.emailAddr != ''){
+              transporter.sendMail(message, function (err, info) {
+                if(err){console.log(err)}
+                  else{console.log(info);}
+              });
+            }
             res.render('gotoRooms', context);
           }
         }
@@ -168,32 +180,6 @@ app.get('/library', (req, res) => {
     }
 
     return res.render('library',{stories: books});
-  });
-});
-
-app.get('/reset',function(req,res,next){
-  console.log('reset')
-  var context = {};
-  var createString = "CREATE TABLE diagnostic(" +
-  "id INT PRIMARY KEY AUTO_INCREMENT," +
-  "text VARCHAR(255) NOT NULL)";
-  mysql.pool.query('DROP TABLE IF EXISTS diagnostic', function(err){
-    if(err){
-      next(err);
-      return;
-    }
-    mysql.pool.query(createString, function(err){
-      if(err){
-        next(err);
-		return;
-      }
-	  mysql.pool.query('INSERT INTO diagnostic (`text`) VALUES ("MySQL is Working!")',function(err){
-	    mysql.pool.query('SELECT * FROM diagnostic', function(err, rows, fields){
-		  context.results = JSON.stringify(rows);
-		  res.render('test',context);
-		});
-	  });
-    });
   });
 });
 
